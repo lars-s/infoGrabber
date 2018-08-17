44d386762ea27bb92d54531879d520a8abf5c8c9
lars-s <lsx@gmx.de>
2018-08-17 17:15:44 +0200

    Update util.js

--- web/assets/js/util.js
+++ web/assets/js/util.js
@@ -68,8 +68,8 @@ function makeFlagsGoldfishLinks(nameCol, foilCol, linkCol) {
         }
         
         // Replace + with - due to new link generation style of MKM and remove duplicate minuses
-        cardname = cardname.replace("+", "-");
-        cardname = cardname.replace("--", "-");
+        cardname = cardname.replace("-", "+");
+        cardname = cardname.replace("++", "+");
 
         var foil = $(this).find("td:nth-child(" + foilCol + ")").find("span").prop("outerHTML");
         if (foil !== undefined &&
